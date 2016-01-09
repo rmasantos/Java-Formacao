abca96a8f0f70d556c41c6dce9fc9f68b89e3d96
rmasantos <rsantos@agap2.pt>
2016-01-09 18:37:09 +0000

    .

--- Java SE 7/src/App.java
+++ Java SE 7/src/App.java
@@ -8,6 +8,6 @@ public class App {
 
 		public static void main(String[] args) {
 			System.out.println("HelloWorld");
-			System.out.println("Paulinho Pacacaio");
+			System.out.println("Ricardo");
 		}
 }
